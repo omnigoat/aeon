eae2e84c485f0fe120edcef1989629015853ff4e
jona <omnigoat@gmail.com>
2013-07-31 15:59:27 +1000

    began addition of semantic analysis

---
+++ include/aeon/semantics/analyse.hpp
@@ -0,0 +1,45 @@
+//=====================================================================
+//
+//
+//
+//=====================================================================
+#ifndef AEON_SEMANTICS_ANALYSE_HPP
+#define AEON_SEMANTICS_ANALYSE_HPP
+//=====================================================================
+#include <cstdint>
+#include <ostream>
+//=====================================================================
+#include <aeon/parsing/parsemes.hpp>
+//=====================================================================
+namespace aeon {
+namespace semantics {
+//=====================================================================
+	
+	struct error_t
+	{
+		std::string text;
+	};
+
+	struct analysis_t
+	{
+		typedef std::vector<error_t> errors_t;
+
+		errors_t errors;
+	};
+
+	namespace analyse
+	{
+		using parsing::parseme_t;
+		using parsing::parseme_ptr;
+		using parsing::parsemes_t;
+
+		auto function(parseme_ptr const&) -> analysis_t;
+	}
+
+//=====================================================================
+} // namespace semantics
+} // namespace aeon
+//=====================================================================
+#endif
+//=====================================================================
+
---
+++ src/semantics/analyse.cpp
@@ -0,0 +1,12 @@
+#include <aeon/semantics/analyse.hpp>
+
+auto aeon::semantics::analyse::function(parseme_ptr const& fn) -> analysis_t
+{
+	//parseme_ptr const& return_type = marshall::function::return_type(fn);
+	//parseme_ptr const& function_body = marshall::function::body(fn);
+
+	// gather up return statements
+	//parsing::raw_parsemes_t return_statements;
+	//parsing::copy_depth_first_if(std::back_inserter(return_statements), function_body->children(), 
+	return analysis_t();
+}
