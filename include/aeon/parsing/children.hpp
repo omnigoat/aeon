5926950610919e53c214f83a6c95b4d66506b2e7
jona <omnigoat@gmail.com>
2013-08-01 12:57:57 +1000

    renamed parsemes_t to children_t, now with actual files

---
+++ include/aeon/parsing/children.hpp
@@ -0,0 +1,73 @@
+//=====================================================================
+//
+//
+//
+//=====================================================================
+#ifndef AEON_PARSING_PARSEMES_HPP
+#define AEON_PARSING_PARSEMES_HPP
+//=====================================================================
+#include <cstdint>
+#include <ostream>
+#include <vector>
+#include <memory>
+//=====================================================================
+namespace aeon {
+namespace parsing {
+//=====================================================================
+	
+	struct parseme_t;
+	typedef std::shared_ptr<parseme_t> parseme_ptr;
+	typedef std::weak_ptr<parseme_t> parseme_wptr;
+
+	struct children_t
+	{
+		typedef std::vector<parseme_ptr>::iterator iterator;
+		typedef std::vector<parseme_ptr>::const_iterator const_iterator;
+
+		children_t();
+
+		auto operator [](int) -> parseme_ptr const&;
+
+		auto size() const -> uint32_t;
+
+		auto front() const -> parseme_ptr const&;
+		auto back() const -> parseme_ptr const&;
+
+		auto begin() const -> const_iterator;
+		auto end() const -> const_iterator;
+		
+		auto replace(const_iterator const&, parseme_ptr const&) -> void;
+		auto detach(const_iterator const&) -> parseme_ptr;
+
+		auto push_back(parseme_ptr const&) -> void;
+		auto pop_back() -> void;
+
+	private:
+		children_t(parseme_t*);
+
+		auto unconst(const_iterator const&) -> iterator;
+
+		parseme_t* owner_;
+		std::vector<parseme_ptr> elements_;
+
+		friend parseme_t;
+	};
+
+
+	namespace detail
+	{
+		auto print_parsemes(std::ostream&, children_t const&, uint32_t = 0) -> std::ostream&;
+	}
+
+	inline std::ostream& operator << (std::ostream& stream, children_t const& x) {
+		stream << "parsemes\n----------\n";
+		return detail::print_parsemes(stream, x);
+	}
+
+//=====================================================================
+} // namespace parsing
+} // namespace aeon
+//=====================================================================
+#endif
+//=====================================================================
+
---
+++ src/parsing/children.cpp
@@ -0,0 +1,91 @@
+#include <aeon/parsing/children.hpp>
+#include <aeon/parsing/parseme.hpp>
+#include <atma/assert.hpp>
+
+using aeon::parsing::parseme_t;
+using aeon::parsing::parseme_ptr;
+using aeon::parsing::parseme_wptr;
+using aeon::parsing::children_t;
+
+children_t::children_t()
+	: owner_()
+{
+}
+
+children_t::children_t(parseme_t* owner)
+	: owner_(owner)
+{
+	ATMA_ASSERT(owner_);
+}
+
+auto children_t::operator[] (int i) -> parseme_ptr const&
+{
+	return elements_[i];
+}
+
+auto children_t::size() const -> uint32_t
+{
+	return elements_.size();
+}
+
+auto children_t::front() const -> parseme_ptr const&
+{
+	return elements_.front();
+}
+
+auto children_t::back() const -> parseme_ptr const&
+{
+	return elements_.back();
+}
+
+auto children_t::begin() const -> const_iterator
+{
+	return elements_.cbegin();
+}
+
+auto children_t::end() const -> const_iterator
+{
+	return elements_.cend();
+}
+
+auto children_t::push_back(parseme_ptr const& x) -> void
+{
+	elements_.push_back(x);
+}
+
+auto children_t::replace(const_iterator const& i, parseme_ptr const& n) -> void
+{
+	parseme_ptr p = *i;
+	p->set_parent(nullptr);
+	n->set_parent(owner_ ? owner_->shared_from_this() : parseme_ptr());
+	*unconst(i) = n;
+}
+
+auto children_t::detach(const_iterator const& i) -> parseme_ptr
+{
+	parseme_ptr p = *i;
+	elements_.erase( unconst(i) );
+	p->set_parent(nullptr);
+	return p;
+}
+
+auto children_t::unconst(const_iterator const& i) -> iterator
+{
+	return elements_.erase(i, i);
+}
+
+auto children_t::pop_back() -> void
+{
+	elements_.back()->set_parent(nullptr);
+	elements_.pop_back();
+}
+
+auto aeon::parsing::detail::print_parsemes(std::ostream& stream, children_t const& xs, uint32_t spaces) -> std::ostream&
+{
+	for (auto const& x : xs) {
+		stream << std::string(spaces, ' ') << *x << std::endl;
+		print_parsemes(stream, x->children(), spaces + 2);
+	}
+
+	return stream;
+}
